9227dd71823fa244c61bc15b731164580ad61305
Ruben Lopez <rubenlopsol86@gmail.com>
2023-07-26 19:15:11 +0200

    add secret

--- ruben_django/settings.py
+++ ruben_django/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-uryc2+9l7*%$g^+d)+*@f9f5)9vq9og872t@ke0pv2h!g418!d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
